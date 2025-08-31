a984af3d155d0819be6e13a27ad8a368dbcfb0d8
coden0w <coden0w@coden0w.io>
2025-08-31 13:29:48 +0200

    Invisible Tabbar View for SwiftUI

---
+++ InvisibleTabbarExample/InvisibleTabbarExample/InvisibleTabbarExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvisibleTabbarExampleApp.swift
+//  InvisibleTabbarExample
+//
+//  Created by alex on 23/11/25.
+//
+
+import SwiftUI
+
+@main
+struct InvisibleTabbarExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
